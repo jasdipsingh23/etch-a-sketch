1a6f3764b32de98d4e3cc4946649fa2ce2ccd589
Jasdip Singh <jasdip.singh@boeing.com>
2022-01-17 13:07:58 -0800

    DOM Testing for appending div

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-ECHO is on.
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
