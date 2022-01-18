6a46bc0226479781baaf2cc70c5fa29c2e710a9b
Jasdip Singh <jasdip.singh@boeing.com>
2022-01-18 10:25:06 -0800

    Created grid with hover functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
+const grid = document.getElementById("grid");
+const DEFAULT_SIZE = 16;
+
+
 function createGrid() {
-    const container = document.getElementById("container");
-    for(let i = 0; i < 16; i++) {
-        let row = document.createElement("div");
-        row.setAttribute("display", "inline-block");
-        row.textContent = "test";
-        for(let j = 0; j < 16; j++) {
-            let column = document.createElement("div");
-            column.textContent = "test2";
-            row.appendChild(column);
-        }
-        container.appendChild(row);
+    grid.style.gridTemplateColumns = `repeat(${DEFAULT_SIZE}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${DEFAULT_SIZE}, 1fr)`
+
+    for(let i = 0; i < DEFAULT_SIZE * DEFAULT_SIZE; i++) {
+        let gridElement = document.createElement("div");
+        gridElement.textContent = "x";
+        gridElement.addEventListener("mouseover", changeColor);
+        grid.appendChild(gridElement);
     }
+
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = `rgb(200, 33, 49)`;
 }
 
 createGrid();
